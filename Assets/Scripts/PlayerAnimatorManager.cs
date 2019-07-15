3c21e4e8d1bde72bb4dc7f67e2f00a104d076d6c
ToshikiHata <toshiki_hata@f4samurai.jp>
2019-07-15 22:31:13 +0900

    5章チュートリアル: ロボットカイルが動き､カメラが追従する所まで

---
+++ Assets/Scripts/PlayerAnimatorManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+using Photon.Pun;
+using Photon.Realtime;
+
+namespace Com.Th1209.PunTutorial
+{
+    [RequireComponent(typeof(Animator))]
+    public class PlayerAnimatorManager : MonoBehaviourPunCallbacks
+    {
+        #region Private Serializable Fields
+
+        [SerializeField]
+        /// <summary>
+        /// 方向転換を完了するまでの時間.
+        /// ※ 値を大きくするほど1回りするまでに時間がかかるため､回転の半径が大きくなる.
+        /// </summary>
+        private float directionDampTime = 0.25f;
+
+        #endregion
+
+
+        #region Private Fields
+
+        private Animator animator;
+
+        #endregion
+
+
+        #region MonoBehaviour CallBacks
+
+        void Start()
+        {
+            animator = GetComponent<Animator>();
+            Debug.Assert(animator != null);
+        }
+
+        void Update()
+        {
+            if (! animator) {
+                return;
+            }
+
+            AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+            if (stateInfo.IsName("Base Layer.Run")) {
+                // 走っている場合だけ､ジャンプ可能にする.
+                if (Input.GetButtonDown("Fire2")) {
+                    animator.SetTrigger("Jump");
+                }
+            }
+
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
+            if (v < 0.0f) {
+                v = 0.0f;
+            }
+            animator.SetFloat("Speed", h * h + v * v);
+            animator.SetFloat("Direction", h, directionDampTime, Time.deltaTime);
+        }
+
+        #endregion
+
+
+        #region Public Methods
+
+        #endregion
+
+
+        #region Private Methods
+
+        #endregion
+    }
+}
