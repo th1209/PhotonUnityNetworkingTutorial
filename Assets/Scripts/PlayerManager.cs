5337e221c276859cd6266380431c96acf15f2258
ToshikiHata <toshiki_hata@f4samurai.jp>
2019-07-15 23:09:59 +0900

    5章チュートリアル: ロボットカイルがビームを発射する所まで

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+using Photon.Pun;
+using Photon.Realtime;
+
+namespace Com.Th1209.PunTutorial
+{
+    [RequireComponent(typeof(Animator))]
+    public class PlayerManager : MonoBehaviourPunCallbacks
+    {
+        #region Private Serializable Fields
+
+        /// <summary>
+        /// ビーム用オブジェクト.
+        /// </summary>
+        [Tooltip("The Beams GameObject to control")]
+        [SerializeField]
+        private GameObject beams;
+
+        #endregion
+
+
+        #region Private Fields
+
+        /// <summary>
+        /// 攻撃中フラグ.
+        /// </summary>
+        private bool isFiring = false;
+
+        #endregion
+
+
+        #region MonoBehaviour CallBacks
+
+        void Awake()
+        {
+            Debug.Assert(beams != null);
+            beams.SetActive(false);
+        }
+
+        void Update()
+        {
+            ProcessInputs();
+
+            if (isFiring != beams.activeSelf) {
+                // ビームのアクティブを切り替える.
+                beams.SetActive(isFiring);
+            }
+        }
+
+        #endregion
+
+
+        #region Custom
+
+        void ProcessInputs()
+        {
+            if (Input.GetButtonDown("Fire1")){
+                if (! isFiring) {
+                    isFiring = true;
+                }
+            }
+
+            if (Input.GetButtonUp("Fire1")){
+                isFiring = false;
+                // if (isFiring) {
+                //     isFiring = false;
+                // }
+            }
+        }
+
+        #endregion
+    }
+}
