ef1d3eb47b1fff4db4cb04f75813a7bb32224007
ToshikiHata <toshiki_hata@f4samurai.jp>
2019-07-15 19:28:02 +0900

    1章チュートリアル: サーバへの接続とルームの作成・参加

---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+namespace Com.Th1209.PunTutorial
+{
+    // public class Launcher : MonoBehaviour
+    public class Launcher : MonoBehaviourPunCallbacks
+    {
+        #region Private Serializable Fields
+        #endregion
+
+
+        #region Private Fields
+
+        [Tooltip("The maximum number of players per room. When a room is full, it can't be joined by new players, and so new room will be created")]
+        [SerializeField]
+        private byte maxPlayersPerRoom = 4;
+
+        /// <summary>
+        /// ゲームバージョン毎に､接続は切り分けられる.
+        /// </summary>
+        string gameVersion = "1";
+
+        #endregion
+
+
+        #region MonoBehaviour CallBacks
+
+        void Awake()
+        {
+            // MasterClientがPhotonNetwork.LoadLevel()を呼び出すと､
+            // 接続済みのプレイヤーは､全員自動的に同じレベルを読み込んでくれる.
+            PhotonNetwork.AutomaticallySyncScene = true;
+        }
+
+        void Start()
+        {
+            Connect();
+        }
+
+        #endregion
+
+
+        #region Public Methods
+
+        public void Connect()
+        {
+            if (PhotonNetwork.IsConnected) {
+                // ランダムにルームへの参加を試みる.
+                // 失敗した場合は､OnJoinRandomFailed()が呼び出される.
+                PhotonNetwork.JoinRandomRoom();
+            } else {
+                // 設定値を用いて､PhotonCloudへ接続する.
+                PhotonNetwork.GameVersion = gameVersion;
+                PhotonNetwork.ConnectUsingSettings();
+            }
+        }
+
+        #endregion
+
+
+        #region MonoBehaviourPunCallbacks Callbacks
+
+        public override void OnConnectedToMaster()
+        {
+            Debug.Log("PUN Basics Tutorial/Launcher: OnConnectedToMaster() was called by PUN");
+        }
+
+        public override void OnDisconnected(DisconnectCause cause)
+        {
+            Debug.LogWarningFormat("PUN Basics Tutorial/Launcher: OnDisconnected() was called by PUN with reason {0}", cause);
+        }
+
+        public override void OnJoinRandomFailed(short returnCode, string message)
+        {
+            Debug.Log("PUN Basics Tutorial/Launcher:OnJoinRandomFailed() was called by PUN. No random room available, so we create one.\nCalling: PhotonNetwork.CreateRoom");
+            // ルーム名と設定値を指定して､ルームを作成する.
+            PhotonNetwork.CreateRoom(null, new RoomOptions{
+                MaxPlayers = maxPlayersPerRoom
+            });
+        }
+
+        public override void OnJoinedRoom()
+        {
+            Debug.Log("PUN Basics Tutorial/Launcher: OnJoinedRoom() called by PUN. Now this client is in a room.");
+        }
+
+        #endregion
+    }
+}
